2bc332489f370abf2c7c0c2a2589b1b50ccec28a
naveenreddy <naveenpeta29@gmail.com>
2025-07-16 23:34:48 +0530

    add Success message file

---
+++ frontend/src/Success.jsx
@@ -0,0 +1,13 @@
+// src/Success.jsx
+import React from 'react';
+
+const Success = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>✅ Payment Successful!</h1>
+      <p>Thank you for your purchase.</p>
+    </div>
+  );
+};
+
+export default Success;
